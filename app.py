980455a282b73532751f40b5407f25098297b0d4
OSUTristanH <harvillt@oregonstate.edu>
2021-10-27 18:38:32 -0500

    Description changes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def root():
 @app.route('/tools/comparison', methods=['POST'])
 def playerComparison():
     # POST to compare two or more players
-    if request.form['function'] == 'compare':
+    if request.form['function'] == 'Compare':
         players = []
         data = {}
         for item in request.form:
@@ -29,7 +29,7 @@ def playerComparison():
 @app.route('/tools/profile', methods=['POST'])
 def playerStats():
     # POST to get a players stats and display them in their profile
-    if request.form['function'] == 'profile':
+    if request.form['function'] == 'Profile':
         player = request.form['player']
         data = ["24", "6'11\"", "214 lbs", "LSU", "1st Round", "2", "5", "5", "0", "433", "488", "-55", "38", "41", "-3", "53", "48", "+5", "4", "3", "+1"]
         return render_template('stats.html', player=player, data=data)
